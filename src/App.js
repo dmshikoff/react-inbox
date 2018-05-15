8011248e41084eb8a63cfb1b75018af171433cfb
dmshikoff <dmshikoff@gmail.com>
2018-05-15 11:35:32 -0700

    completed starred, read, unread and delete"

--- src/App.js
+++ src/App.js
@@ -14,111 +14,109 @@ class App extends Component {
     super(props)
 
     this.state = {
-        messages: []
+      messages: []
     }
-}
+  }
 
-getMessages() {
-  axios.get('http://localhost:8082/api/messages')
-  .then(data => {
-    this.setState({messages: data.data})
-  })
-}
+  getMessages() {
+    axios.get('http://localhost:8082/api/messages')
+      .then(data => {
+        this.setState({ messages: data.data })
+      })
+  }
 
-componentDidMount(){
-  this.getMessages()
-}
+  componentDidMount() {
+    this.getMessages()
+  }
 
-handleSelected = (id, selected) => {
-  const newmessages = this.state.messages.map(ele => ele.id === id ? {...ele, selected} : {...ele})
+  getId() {
+    const idArray = this.state.messages
+      .filter(message => (message.selected === true))
+      .map(message => (message.id))
 
-  this.setState( { messages: newmessages })
-  
-}
+    return idArray
+  }
 
-handleStarred =(id) => {
-  const newmessages = this.state.messages.map(ele => ele.id === id ? {...ele, starred: !ele.starred} : {...ele})
+  handleSelected = (id, selected) => {
+    const newmessages = this.state.messages.map(ele => ele.id === id ? { ...ele, selected } : { ...ele })
+    this.setState({ messages: newmessages })
+  }
 
-  this.setState( { messages: newmessages })
+  handleStarred = (id) => {
+    axios.patch('http://localhost:8082/api/messages', { messageIds: [id], command: 'star' })
+      .then(response => {
+        this.getMessages()
+      })
+  }
 
-}
+  handleDelete = () => {
+    axios.patch('http://localhost:8082/api/messages', { messageIds: this.getId(), command: 'delete' })
+      .then(response => {
+        this.getMessages()
+      })
+  }
 
-handleRead=(id) => {
-  const newmessages = this.state.messages.map(ele => ele.id === id ? {...ele, read: true} : {...ele})
+  handleMarkAsRead = () => {
+    axios.patch('http://localhost:8082/api/messages', { messageIds: this.getId(), command: 'read', read: true })
+      .then(response => {
+        this.getMessages()
+      })
+  }
 
-  this.setState( { messages: newmessages })
+  handleMarkAsUnread = () => {
+    axios.patch('http://localhost:8082/api/messages', { messageIds: this.getId(), command: 'read', read: false })
+      .then(response => {
+        this.getMessages()
+      })
+  }
 
-}
+  handleApplyLabel = event => {
+    let messageList = this.state.messages
 
-handleCheckAll=()=> {
-  const isEverythingSelected = this.state.messages.every(data => data.selected)
-  const messageList = this.state.messages
+    let newmessages = messageList.map(message => {
 
-  let newmessages = isEverythingSelected 
-    ? messageList.map(({selected, ...message}) => ({...message})) 
-    : messageList.map(message => ({...message, selected: true}))
+      message.selected && (message.labels = [...message.labels, event.target.value])
 
-  this.setState( { messages: newmessages })
-}
+      return message
+    })
+    this.setState({ messages: newmessages })
+  }
 
-handleMarkAsRead=()=> {
-  let messageList = this.state.messages
+  handleRemoveLabel = event => {
+    let messageList = this.state.messages
 
-  let newmessages = messageList.map(message => {
+    let newmessages = messageList.map(message => {
 
-    return message.selected ? { ...message, read: true} : {...message}
-  })
+      message.selected && (message.labels = message.labels.filter(ele => (ele !== event.target.value)))
 
-  this.setState( { messages: newmessages })
-}
+      return message
+    })
+    this.setState({ messages: newmessages })
+  }
 
-handleMarkAsUnread=()=> {
-  let messageList = this.state.messages
+  handleRead = (id) => {
+    const newmessages = this.state.messages.map(ele => ele.id === id ? { ...ele, read: true } : { ...ele })
 
-  let newmessages = messageList.map(message => {
+    this.setState({ messages: newmessages })
 
-    return message.selected ? { ...message, read: false} : {...message}
-  })
+  }
 
-  this.setState( { messages: newmessages })
-}
+  handleCheckAll = () => {
+    const isEverythingSelected = this.state.messages.every(data => data.selected)
+    const messageList = this.state.messages
 
-handleApplyLabel= event=> {
-  let messageList = this.state.messages
+    let newmessages = isEverythingSelected
+      ? messageList.map(({ selected, ...message }) => ({ ...message }))
+      : messageList.map(message => ({ ...message, selected: true }))
 
-  let newmessages = messageList.map(message => {
-
-    message.selected && (message.labels = [...message.labels, event.target.value])
-    
-    return message
-  })
-this.setState( {messages: newmessages})
-}
-
-handleRemoveLabel= event=> {
-  let messageList = this.state.messages
-
-  let newmessages = messageList.map(message => {
-    
-    message.selected && (message.labels = message.labels.filter(ele => (ele !== event.target.value)))
-
-    return message
-  })
-  this.setState( {messages: newmessages})
-}
-
-handleDelete= () => {
-  let newmessages = this.state.messages.filter(message => {
-    return !message.selected
-  })
-  this.setState( {messages : newmessages})
-}
+    this.setState({ messages: newmessages })
+  }
 
   render() {
     return (
       <div className='container-fluid'>
-        <Toolbar messageData={ this.state.messages } handleCheckAll={this.handleCheckAll} handleMarkAsRead={this.handleMarkAsRead} handleMarkAsUnread={this.handleMarkAsUnread} handleApplyLabel={this.handleApplyLabel} handleRemoveLabel={this.handleRemoveLabel} handleDelete={this.handleDelete}/>
-        <Messages messageData={ this.state.messages } handleSelected={this.handleSelected} handleStarred={this.handleStarred} handleRead={this.handleRead}/>
+        <Toolbar messageData={this.state.messages} handleCheckAll={this.handleCheckAll} handleMarkAsRead={this.handleMarkAsRead} handleMarkAsUnread={this.handleMarkAsUnread} handleApplyLabel={this.handleApplyLabel} handleRemoveLabel={this.handleRemoveLabel} handleDelete={this.handleDelete} />
+        <Messages messageData={this.state.messages} handleSelected={this.handleSelected} handleStarred={this.handleStarred} handleRead={this.handleRead} />
       </div>
     );
   }
